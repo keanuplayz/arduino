b7ac2f92bb231adb6f14b2a83998f14e3891c35e
Keanu <keanutimmermans8@gmail.com>
2020-03-13 14:13:11 +0100

    Added proper indentation

--- main.cs
+++ main.cs
@@ -26,24 +26,24 @@ void loop()
   float temperature = (voltage - .5) * 100;
   Serial.println(temperature);
   
-  if(temperature < baselineTemp){
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  }else if(temperature >= baselineTemp+2 &&
-	temperature < baselineTemp+4){
-	digitalWrite(2, HIGH);
-	digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
-  }else if(temperature >= baselineTemp+4 &&
-	temperature < baselineTemp+6){
-	digitalWrite(2, HIGH);
-	digitalWrite(3, HIGH);
-	digitalWrite(4, LOW);
-  }else if(temperature >= baselineTemp+6){
-	digitalWrite(2, HIGH);
-	digitalWrite(3, HIGH);
-	digitalWrite(4, HIGH);
+  if (temperature < baselineTemp) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp+2 && temperature < baselineTemp+4) {
+  	digitalWrite(2, HIGH);
+  	digitalWrite(3, LOW);
+	  digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp+4 && temperature < baselineTemp+6) {
+  	digitalWrite(2, HIGH);
+  	digitalWrite(3, HIGH);
+  	digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp+6) {
+  	digitalWrite(2, HIGH);
+	  digitalWrite(3, HIGH);
+  	digitalWrite(4, HIGH);
   }
+
 	delay(1);
+  
 }
