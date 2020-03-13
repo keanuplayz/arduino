681e13f65122eb89ab626e216de10c9b848484ab
Keanu <keanutimmermans8@gmail.com>
2020-03-13 14:14:20 +0100

    Added proper indentation.

--- main.cs
+++ main.cs
@@ -10,18 +10,18 @@ void loop(){
   switchState = digitalRead(2);
   if (switchState == LOW) { // Button is not pressed
     digitalWrite(3, HIGH); // green LED
-	digitalWrite(4, LOW); // red LED
-	digitalWrite(5, LOW); // red LED
+  	digitalWrite(4, LOW); // red LED
+	  digitalWrite(5, LOW); // red LED
   } else { // Button is pressed
     digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
-	digitalWrite(5, HIGH);
+  	digitalWrite(4, LOW);
+  	digitalWrite(5, HIGH);
     
     delay(250); // wait for a quarter second
 
     // toggle the LEDs
-	digitalWrite(4, HIGH);
-	digitalWrite(5, LOW);
-	delay(250); // wait for a quarter second
+  	digitalWrite(4, HIGH);
+  	digitalWrite(5, LOW);
+  	delay(250); // wait for a quarter second
   }
 }
