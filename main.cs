4827e43df6c378c79c73e950c7ac44f8f842f65d
Keanu <keanutimmermans8@gmail.com>
2020-03-13 14:11:30 +0100

    Added code and brd

--- main.cs
+++ main.cs
@@ -1,27 +1,49 @@
-int switchState = 0;
-void setup(){
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+const int sensorPin = A0;
+const float baselineTemp = 20.0;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  for(int pinNumber = 2; pinNumber<5; pinNumber++){
+	pinMode(pinNumber,OUTPUT);
+	digitalWrite(pinNumber, LOW);
+  }
+  
 }
 
-void loop(){
-  switchState = digitalRead(2);
-  if (switchState == LOW) { // Button is not pressed
-    digitalWrite(3, HIGH); // green LED
-	digitalWrite(4, LOW); // red LED
-	digitalWrite(5, LOW); // red LED
-  } else { // Button is pressed
-    digitalWrite(3, LOW);
+void loop()
+{
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+  
+  float voltage = (sensorVal/1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+  
+  if(temperature < baselineTemp){
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  }else if(temperature >= baselineTemp+2 &&
+	temperature < baselineTemp+4){
+	digitalWrite(2, HIGH);
+	digitalWrite(3, LOW);
 	digitalWrite(4, LOW);
-	digitalWrite(5, HIGH);
-    
-    delay(250); // wait for a quarter second
-
-    // toggle the LEDs
+  }else if(temperature >= baselineTemp+4 &&
+	temperature < baselineTemp+6){
+	digitalWrite(2, HIGH);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, LOW);
+  }else if(temperature >= baselineTemp+6){
+	digitalWrite(2, HIGH);
+	digitalWrite(3, HIGH);
 	digitalWrite(4, HIGH);
-	digitalWrite(5, LOW);
-	delay(250); // wait for a quarter second
   }
+	delay(1);
 }
