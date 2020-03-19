de094f9a5cd031b81f642b90c4927785b80a10bb
Keanu <keanutimmermans8@gmail.com>
2020-03-19 15:03:27 +0100

    Added code and brd

--- main.cs
+++ main.cs
@@ -1,27 +1,59 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+const int switchPin = 6;
 int switchState = 0;
-void setup(){
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+int prevSwitchState = 0;
+int reply;
+
+void setup() {
+ lcd.begin(16, 2);
+ pinMode(switchPin, INPUT);
+
+ lcd.print("Ask the");
+ lcd.setCursor(0, 1);
+ lcd.print("Crystal ball!");
 }
 
-void loop(){
-  switchState = digitalRead(2);
-  if (switchState == LOW) { // Button is not pressed
-    digitalWrite(3, HIGH); // green LED
-  	digitalWrite(4, LOW); // red LED
-	  digitalWrite(5, LOW); // red LED
-  } else { // Button is pressed
-    digitalWrite(3, LOW);
-  	digitalWrite(4, LOW);
-  	digitalWrite(5, HIGH);
-    
-    delay(250); // wait for a quarter second
+void loop() {
+ switchState = digitalRead(switchPin);
 
-    // toggle the LEDs
-  	digitalWrite(4, HIGH);
-  	digitalWrite(5, LOW);
-  	delay(250); // wait for a quarter second
+ if (switchState != prevSwitchState) {
+  if (switchState == LOW) {
+   reply = random(8);
+
+   lcd.clear();
+   lcd.setCursor(0, 0);
+   lcd.print("The ball says: ");
+   lcd.setCursor(0, 1);
+
+   switch (reply) {
+    case 0:
+     lcd.print("Yes");
+     break;
+    case 1:
+     lcd.print("Most likely");
+     break;
+    case 2:
+     lcd.print("Certainly");
+     break;
+    case 3:
+     lcd.print("Outlook good");
+     break;
+    case 4:
+     lcd.print("Unsure");
+     break;
+    case 5:
+     lcd.print("Ask again");
+     break;
+    case 6:
+     lcd.print("Doubtful");
+     break;
+    case 7:
+     lcd.print("No");
+     break;
+   }
   }
+ }
+ prevSwitchState = switchState;
 }
