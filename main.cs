d33b66b76f2177e89fdc9175eba5752e5a957468
Keanu <keanutimmermans8@gmail.com>
2020-03-19 13:56:55 +0100

    Added code and brd

--- main.cs
+++ main.cs
@@ -1,27 +1,36 @@
+const int switchPin = 8;
+unsigned long previousTime = 0;
 int switchState = 0;
-void setup(){
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+int prevSwitchState = 0;
+int led = 2;
+long interval = 600000;
+void setup() {
+ for (int x = 2; x < 8; x++) {
+  pinMode(x, OUTPUT);
+ }
+ pinMode(switchPin, INPUT);
+
 }
+void loop() {
+ unsigned long currentTime = millis();
 
-void loop(){
-  switchState = digitalRead(2);
-  if (switchState == LOW) { // Button is not pressed
-    digitalWrite(3, HIGH); // green LED
-  	digitalWrite(4, LOW); // red LED
-	  digitalWrite(5, LOW); // red LED
-  } else { // Button is pressed
-    digitalWrite(3, LOW);
-  	digitalWrite(4, LOW);
-  	digitalWrite(5, HIGH);
-    
-    delay(250); // wait for a quarter second
+ if (currentTime - previousTime > interval) {
+  previousTime = currentTime;
 
-    // toggle the LEDs
-  	digitalWrite(4, HIGH);
-  	digitalWrite(5, LOW);
-  	delay(250); // wait for a quarter second
+  digitalWrite(led, HIGH);
+  led++;
+  if (led == 7) {
+
   }
+ }
+ switchState = digitalRead(switchPin);
+
+ if (switchState != prevSwitchState) {
+  for (int x = 2; x < 8; x++) {
+   digitalWrite(x, LOW);
+  }
+  led = 2;
+  previousTime = currentTime;
+ }
+ prevSwitchState = switchState;
 }
