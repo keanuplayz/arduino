f147f91083374feb2993acc87fb0cb45c1eb0204
Keanu <keanutimmermans8@gmail.com>
2020-03-19 14:09:38 +0100

    Added code and brd

--- main.cs
+++ main.cs
@@ -1,27 +1,18 @@
+const int switchPin = 2;
+const int motorPin = 9;
 int switchState = 0;
-void setup(){
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+
+void setup () {
+  pinMode (motorPin, OUTPUT);
+  pinMode (switchPin, INPUT);
 }
 
-void loop(){
-  switchState = digitalRead(2);
-  if (switchState == LOW) { // Button is not pressed
-    digitalWrite(3, HIGH); // green LED
-  	digitalWrite(4, LOW); // red LED
-	  digitalWrite(5, LOW); // red LED
-  } else { // Button is pressed
-    digitalWrite(3, LOW);
-  	digitalWrite(4, LOW);
-  	digitalWrite(5, HIGH);
-    
-    delay(250); // wait for a quarter second
+void loop () {
+  switchState = digitalRead (switchPin);
 
-    // toggle the LEDs
-  	digitalWrite(4, HIGH);
-  	digitalWrite(5, LOW);
-  	delay(250); // wait for a quarter second
+  if (switchState == HIGH) {
+    digitalWrite (motorPin, HIGH);
+  } else {
+    digitalWrite(motorPin, LOW)
   }
 }
